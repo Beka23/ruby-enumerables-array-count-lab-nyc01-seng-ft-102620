1064fcf9f3b8507bebcd2c3b70ffb2b9065d4c12
Beksultan <beksultankaiypov@gmail.com>
2020-10-14 18:29:10 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,15 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
   
-
+  total = 0 
+  index = 0 
+  while index < array.count do 
+    if array[index].class == String
+      total += 1 
+    end
+    index += 1 
+  end
+total
 end
 
 
